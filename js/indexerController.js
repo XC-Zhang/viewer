656a3737b6676afac54322f737c4b3d6ba84168e
xc-zhang <studentzxc@163.com>
2014-12-05 13:39:16 +0800

    Remove some animation

--- js/indexerController.js
+++ js/indexerController.js
@@ -5,28 +5,11 @@ angular.module('Viewer')
 		restrict: 'E',
 		templateUrl: 'partials/indexer.html',
 		controller: function ($scope, $timeout) {
-			$scope.expand = function () {
-				if ($scope.$root.current.section < 0)
-					return;
-				$scope.$root.expanded = true;
-			};
 			$scope.siteSelected = function (index) {
 				if (index === $scope.$root.current.site)
 					return;
-				$timeout(function () {
-					$scope.$root.current.site = index;
-				}, 500);
+				$scope.$root.current.site = index;
 			};
-			$scope.$root.$watch('current.section', function (newValue) {
-				if (newValue < 0)
-					return;
-				$scope.$root.expanded = true;
-			});
-			$scope.$root.$watch('current.site', function (newValue) {
-				if (newValue < 0)
-					return;
-				$scope.$root.expanded = false;
-			});
 		}
 	};
 }])
