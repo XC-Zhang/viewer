d08da6340a32d61f6242cda83ffecc68bf059b4d
xc-zhang <studentzxc@163.com>
2014-12-26 19:58:59 +0800

    Bug fixed

--- js/ringNumberController.js
+++ js/ringNumberController.js
@@ -6,10 +6,17 @@ angular.module('Viewer')
 		templateUrl: 'partials/ringnumber.html',
 		controller: function ($scope, $mdToast) {
 			var refresh = function (obj) {
-				$scope.ringNumber = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Number | "无";
-				$scope.mileage = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Mileage | "无";
-				$scope.warning = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Warning | false;
-				$scope.date = obj.date.replace(/\//g, '-') | "无";
+				if ($scope.$root.ringNumbers) {
+					$scope.ringNumber = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Number | "无";
+					$scope.mileage = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Mileage | "无";
+					$scope.warning = $scope.$root.ringNumbers[$scope.$root.current.section][$scope.$root.current.site].Warning | false;
+				}
+				else {
+					$scope.ringNumber = "无";
+					$scope.mileage = "无";
+					$scope.warning = false;					
+				}
+				$scope.date = obj.date.replace(/\//g, '-');
 			};
 			$scope.warning = false;
 			$scope.$root.$watch('current.site', function (newValue) {
